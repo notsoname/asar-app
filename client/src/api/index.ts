59fe883161150352263556fdb8653cc1e13a00e7
Asar <notsoname@gmail.com>
2022-06-14 18:17:59 +0600

    f

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const API_URL = `http://localhost:5000/api`
 
 const api = axios.create({
-  withCredentials: true,
+  // withCredentials: true,
   baseURL: API_URL
 })
 
--- client/src/pages/homePage/index.tsx
+++ client/src/pages/homePage/index.tsx
@@ -1,6 +1,9 @@
 import Header from '../../components/header';
+import { useAppSelector } from '../../hooks';
 
 export default function HomePage() {
+  const {users} = useAppSelector(state => state.AuthReducer)
+  console.log(users)
   return (
     <div>
       Hello
