eb9bc4905bb97f57be5154c616898ae7c442368a
Asar <you@example.com>
2022-07-11 22:53:18 +0600

    delete

--- client/src/components/home/index.tsx
+++ client/src/components/home/index.tsx
@@ -4,24 +4,6 @@ import { createPost } from "../../redux/postsReducer/actionCreators";
 import PostItems from "../posts/postsItems";
 
 const Home: FC = () => {
-    const dispatch = useAppDispatch()
-    const [file, setFile] = useState<File>()
-    const handleImageChange = function (e: React.ChangeEvent<HTMLInputElement>) {
-        const fileList = e.target.files;
-        if (!fileList) return;
-        setFile(fileList[0]);
-    };
-
-    const uploadFile = function (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) {
-        if (file) {
-            const formData = new FormData();
-            formData.append("image", file, file.name);
-            formData.append("title", "hello")
-            formData.append("description", "description")
-            dispatch(createPost(formData))
-        }
-    };
-
     return (
         <div>
             <PostItems/>
